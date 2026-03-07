de8b0da883a2a6841f1c4ad6557e1b71bcff5896
Angela Medina <angelamedina03.2016@gmail.com>
2026-03-07 16:52:21 -0500

    carpeta donde se alamcena los archivos

--- problema5/servidor.py
+++ problema5/servidor.py
@@ -6,3 +6,7 @@ import hashlib
 HOST = "127.0.0.1"
 PORT = 5000
 BUFFER = 4096
+
+# Carpeta donde se almacenarán los archivos del servidor
+BASE_DIR = "server_files"
+os.makedirs(BASE_DIR, exist_ok=True)  # Crear carpeta si no existe
