68a3a372bb7282d7bcda0fe201486e327029b65f
Angela Medina <angelamedina03.2016@gmail.com>
2026-03-08 16:39:22 -0500

    asosciar dirección y puerto

--- problema7/proxy.py
+++ problema7/proxy.py
@@ -135,5 +135,8 @@ def start():
     
     # Crear socket TCP
     proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Asociar dirección y puerto
+    proxy.bind((HOST, PORT))
 
 
