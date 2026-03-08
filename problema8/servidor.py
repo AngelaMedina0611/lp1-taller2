0b1ae7eef3b20ed2ef28ffd8a57acb51fbef2da4
Angela Medina <angelamedina03.2016@gmail.com>
2026-03-08 18:10:25 -0500

    verifica si existe un ganador o empate

--- problema8/servidor.py
+++ problema8/servidor.py
@@ -42,3 +42,13 @@ def broadcast(msg):
             client.sendall((msg + "\n").encode())
         except:
             pass
+
+def check_winner():
+    """
+    Verifica si existe un ganador o empate.
+    """
+    wins = [
+        (0,1,2),(3,4,5),(6,7,8),  # filas
+        (0,3,6),(1,4,7),(2,5,8),  # columnas
+        (0,4,8),(2,4,6)           # diagonales
+    ]
