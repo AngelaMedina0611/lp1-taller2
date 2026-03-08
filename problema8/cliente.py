e5280dab602b14a8fb78366f36e9f9fbfa263dab
Angela Medina <angelamedina03.2016@gmail.com>
2026-03-08 18:58:20 -0500

    hilo recibe mensajes del servidor

--- problema8/cliente.py
+++ problema8/cliente.py
@@ -4,3 +4,17 @@ import threading
 # Dirección del servidor
 HOST = "127.0.0.1"
 PORT = 5000
+
+def receive(sock):
+    """
+    Hilo encargado de recibir mensajes del servidor
+    para que el cliente pueda escribir y recibir al mismo tiempo.
+    """
+    while True:
+        try:
+            msg = sock.recv(1024).decode()
+            if not msg:
+                break
+            print(msg)
+        except:
+            break
