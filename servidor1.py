585b5fe54f79d0bdeffd911a1c5511fbc473d42c
Angela Medina <angelamedina03.2016@gmail.com>
2026-03-08 21:01:26 -0500

    almacenar mensajes recibidos

--- servidor1.py
+++ servidor1.py
@@ -8,3 +8,6 @@ PORT = 5000
 # Dirección del servidor secundario donde se replicarán los datos
 REPLICA_HOST = "127.0.0.1"
 REPLICA_PORT = 6000
+
+# Lista para almacenar los mensajes recibidos
+messages = []
