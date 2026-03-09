c13b9c007b9d31169cfe394b12ca7e69b7f1fe32
Angela Medina <angelamedina03.2016@gmail.com>
2026-03-08 21:00:31 -0500

    agrega servidor1

---
+++ servidor1.py
@@ -0,0 +1,10 @@
+import socket
+import threading
+
+# Dirección y puerto del servidor principal
+HOST = "127.0.0.1"
+PORT = 5000
+
+# Dirección del servidor secundario donde se replicarán los datos
+REPLICA_HOST = "127.0.0.1"
+REPLICA_PORT = 6000
