aed52d24e784c4123903ac37f62290ee13e951ef
Angela Medina <angelamedina03.2016@gmail.com>
2026-03-08 22:09:20 -0500

    conectarse al servidor

--- cliente.py
+++ cliente.py
@@ -6,3 +6,8 @@ PORT = 5000
 
 # Crear socket TCP
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conectarse al servidor
+client.connect((HOST, PORT))
+
+print("Conectado al servidor")
